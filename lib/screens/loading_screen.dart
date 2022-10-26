1c7d5027671c770faa2b647197e15d4c3fa0f84a
vinicius.ochner <vinicius.ochner@fulltechpro.com.br>
2022-10-26 20:23:38 -0300

    primeira parte

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../services/location.dart';
+import 'dart:convert';
+
+import '../services/networking.dart';
+import 'location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,17 +14,43 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late double latitude;
+  late double longitude;
+  void pushToLocationScreen() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return const LocationScreen();
+    }));
+  }
+
+  String apiKey = 'b7fe8eb4281863323d20fd563aab6c45';
+
+  void getData() async {
+    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/'
+        'data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+  }
+
+  Future<void> getLocation() async {
+    var location = Location();
+    await location.getCurrentLocation();
+
+    latitude = location.latitude!;
+    longitude = location.longitude!;
+
+    getData();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    var location = new Location();
+    location.getCurrentLocation();
+    getData();
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // obtém a localização atual
-          },
-          child: const Text('Obter Localização'),
-        ),
-      ),
-    );
+    getData();
+    return Scaffold();
   }
 }
